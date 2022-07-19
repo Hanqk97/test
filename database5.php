f143895f4348617a5b43cd3b756b070a339f9cf4
Hnaqk97 <1270640138@qq.com>
2022-07-19 17:07:33 -0500

    111

--- database5.php
+++ database5.php
@@ -1,7 +1,7 @@
 <?php
 // Content of database.php
  
-$mysqli = new mysqli('localhost', 'shin', '123789', 'calendar');
+$mysqli = new mysqli('localhost', 'wustl_inst', 'wustl_inst', 'M5GROUP');
  
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
